44f1c19a6f6dd28e5d60f022a2d63afa377276c2
Arkadiusz Bachorski <60391032+arkadiuszbachorski@users.noreply.github.com>
2026-03-24 10:53:20 +0100

    Improve admin panel

--- src/routes/~_dashboard/~admin/~index.tsx
+++ src/routes/~_dashboard/~admin/~index.tsx
@@ -6,7 +6,12 @@
 // SPDX-License-Identifier: MIT
 //
 
-import { PageTitle } from "@stanfordspezi/spezi-web-design-system/molecules/DashboardLayout";
+import {
+  Card,
+  PageTitle,
+  CardHeader,
+  CardTitle,
+} from "@stanfordspezi/spezi-web-design-system";
 import { createFileRoute } from "@tanstack/react-router";
 import { MonitorCog } from "lucide-react";
 import { UserType } from "spezi-firebase-template/models";
@@ -17,7 +22,17 @@ import { DashboardLayout } from "../DashboardLayout";
 const AdminPage = () => (
   <DashboardLayout title={<PageTitle title="Admin" icon={<MonitorCog />} />}>
     <title>{getTitle("Admin")}</title>
-    <p className="text-muted-foreground">Admin panel</p>
+    <Card className="max-w-lg">
+      <CardHeader>
+        <CardTitle>Admin panel</CardTitle>
+      </CardHeader>
+      <div className="p-5 pt-0">
+        <p>
+          This page is only visible to users with administrator privileges.
+          Manage application settings and access administrative tools from here.
+        </p>
+      </div>
+    </Card>
   </DashboardLayout>
 );
 
